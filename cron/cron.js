30bd5fb08ba5dbe14d46325b1b14f13fe311995e
ripupo88 <ripupo88@gmail.com>
2019-09-07 16:18:39 +0100

    v0.2.3

--- cron/cron.js
+++ cron/cron.js
@@ -5,42 +5,39 @@ const jspdf = require("../jspdf/pdf");
 const mongo = require("../mongo/mongodb");
 
 new CronJob(
-    "0 30 8 1 * *",
-    async() => {
-        let empleados = [];
-        let informe = [];
+   "0 30 8 1 * *",
+   async () => {
+      let empleados = [];
+      let informe = [];
 
-        empleados = await mongo.f_obten_empleados();
+      empleados = await mongo.f_obten_empleados();
 
-        empleados.forEach(async empleado => {
-            informe = await mongo.f_obten_informe(empleado);
-            if (informe[0] != undefined) {
-                jspdf.f_crea_pdf(informe, empleado);
-            }
-        });
-    },
-    null,
-    true,
-    "Europe/London"
+      empleados.forEach(async empleado => {
+         informe = await mongo.f_obten_informe(empleado);
+         if (informe[0] != undefined) {
+            jspdf.f_crea_pdf(informe, empleado);
+         }
+      });
+   },
+   null,
+   true,
+   "Europe/London"
 );
 
 new CronJob(
-    "0 */5 * * * *",
-    async() => {
-        try {
-            console.log('sincro');
-            let jornadas_abiertas = await mongo.f_fin_jornada()
-            console.log(jornadas_abiertas);
-            jornadas_abiertas.forEach(regsitro => {
-                mongo.f_avisa_empleado(regsitro.empleado);
-            });
-        } catch (e) {
-            console.log(e);
-        }
+   "0 */5 * * * *",
+   async () => {
+      try {
+         let jornadas_abiertas = await mongo.f_fin_jornada();
 
-
-    },
-    null,
-    true,
-    "Europe/London"
+         jornadas_abiertas.forEach(registro => {
+            mongo.f_avisa_empleado(registro.empleado);
+         });
+      } catch (e) {
+         console.log(e);
+      }
+   },
+   null,
+   true,
+   "Europe/London"
 );
