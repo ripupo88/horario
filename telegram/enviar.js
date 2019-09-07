82242c185e66a99581a6f036fd0a867b0a0dc26a
Richar <rpupo88@gmail.com>
2019-09-07 05:00:24 +0100

    ajuste

--- telegram/enviar.js
+++ telegram/enviar.js
@@ -3,23 +3,23 @@ const telegram_config = require("../privado/telegram.config");
 const fs = require("fs");
 
 var api = new telegram({
-   token: telegram_config.telegram_config.token
+    token: telegram_config.telegram_config.token
 });
 
 let f_manda_mensaje = (chat_id, text) => {
-   api.sendMessage({
-      chat_id,
-      text
-   });
+    api.sendMessage({
+        chat_id,
+        text
+    });
 };
 
 let f_enviar_doc = doc => {
-   let document = fs.createReadStream(`./informes/${doc}.pdf`);
+    let document = fs.createReadStream(`./informes/${doc}.pdf`);
 
-   api.sendDocument({
-      chat_id: 823806648,
-      document
-   });
+    api.sendDocument({
+        chat_id: 880957543, //Raul
+        document
+    });
 };
 
 module.exports = { f_manda_mensaje, f_enviar_doc };
