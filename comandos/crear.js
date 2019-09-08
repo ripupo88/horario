9aaf827a7ef9a7e58be3f3ac3e3ecbf7399690dc
Richar <rpupo88@gmail.com>
2019-09-08 05:01:17 +0100

    v0.2.6 mejorando codigo

--- comandos/crear.js
+++ comandos/crear.js
@@ -2,65 +2,34 @@ const mongo = require('../mongo/mongodb');
 const enviar = require('../telegram/enviar');
 const confirmar = require('../telegram/confirmacion');
 
+let f_procesa_crear = async message => {
+  try {
+    let creador_mensaje = await mongo.f_confirma_telegram_id(message.from.id);
 
-let f_procesa_crear = (message) => {
-    mongo.f_confirma_telegram_id(message.from.id)
-        .then(creador_mensaje => {
+    if (creador_mensaje[0].role == 'ADMIN_ROLE') {
+      let mensaje_separado = message.text.split(',');
 
-            if (creador_mensaje[0] != undefined) {
-                if (creador_mensaje[0].role == "ADMIN_ROLE") {
+      let nuevo_usuario = {
+        nombre: mensaje_separado[0].replace(/\/crear /g, '').trim(),
+        nif: mensaje_separado[1].trim(),
+        alias: mensaje_separado[2].trim(),
+        telegram_id: message.reply_to_message.from.id
+      };
+      let texto_confirmacion = `¿confirmas la creacion de ${nuevo_usuario.alias}\nnombre${nuevo_usuario.nombre}\nNIF${nuevo_usuario.nif}\n`;
+      await confirmar.f_confirmacion(message, texto_confirmacion).catch(err => {
+        console.log(err);
+        throw new Error('El usuario no se ha creado, ha ocurrido un error');
+      });
 
-                    let mensaje_separado = message.text.split(",");
-                    console.log(message);
-                    let nombre = mensaje_separado[0].replace(/\/crear /g, "").trim();
-                    let nif = mensaje_separado[1].trim();
-                    let alias = mensaje_separado[2].trim();
-                    let telegram_id = message.reply_to_message.from.id;
+      let usuario = await mongo.f_nuevo_usuario(nuevo_usuario);
 
-                    let nuevo_usuario = {
-                        nombre,
-                        nif,
-                        alias,
-                        telegram_id
-                    }
-                    let texto_confirmacion = `¿confirmas la creacion de ${alias}\nnombre${nombre}\nNIF${nif}\n`;
-                    confirmar.f_confirmacion(message, texto_confirmacion)
-                        .then(data => {
+      enviar.f_manda_mensaje(message.chat.id, `${usuario.alias} añadido`);
+    } else {
+      throw new Error('No tiene privilegios para realizar esta operación');
+    }
+  } catch (e) {
+    enviar.f_manda_mensaje(message.chat.id, e.toString());
+  }
+};
 
-                            console.log(data);
-                            mongo.f_nuevo_usuario(nuevo_usuario)
-                                .then(usuario => {
-                                    console.log(usuario);
-                                    enviar.f_manda_mensaje(message.chat.id, `El usuario ${usuario.alias} ha sido creado`);
-                                })
-                                .catch(err => {
-                                    console.log(err);
-                                    enviar.f_manda_mensaje(message.chat.id, err);
-                                });
-
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            enviar.f_manda_mensaje(message.chat.id, 'El usuario no se ha creado, ha ocurrido un error');
-                        })
-
-
-                } else {
-                    enviar.f_manda_mensaje(message.chat.id, 'No tiene privilegios para realizar esta operación');
-                }
-            } else {
-                enviar.f_manda_mensaje(message.chat.id, 'Su ID no aparece en nuestra base de datos');
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            enviar.f_manda_mensaje(message.chat.id, 'Ha ocurrido un error');
-        });
-}
-
-let f_nombre = (nombre) => {
-    console.log('En creando nombre');
-    console.log("creando", nombre);
-}
-
-module.exports = { f_procesa_crear, f_nombre };
+module.exports = { f_procesa_crear };
