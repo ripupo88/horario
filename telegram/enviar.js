cfc53802f223d6754b60f6b9b8fcc3bb31f3e1cd
ripupo88 <ripupo88@gmail.com>
2019-09-13 01:41:55 +0100

    v1.0.0
    versión para lanzar

--- telegram/enviar.js
+++ telegram/enviar.js
@@ -1,25 +1,25 @@
-const telegram = require("telegram-bot-api");
-const telegram_config = require("../privado/telegram.config");
-const fs = require("fs");
+const telegram = require('telegram-bot-api');
+const telegram_config = require('../privado/telegram.config');
+const fs = require('fs');
 
 var api = new telegram({
-  token: telegram_config.telegram_config.token
+    token: telegram_config.telegram_config.token
 });
 
 let f_manda_mensaje = (chat_id, text) => {
-  api.sendMessage({
-    chat_id,
-    text
-  });
+    api.sendMessage({
+        chat_id,
+        text
+    });
 };
 
 let f_enviar_doc = (doc, chat_id) => {
-  let document = fs.createReadStream(`./informes/${doc}.pdf`);
+    let document = fs.createReadStream(`./informes/${doc}.pdf`);
 
-  api.sendDocument({
-    chat_id,
-    document: document.path
-  });
+    api.sendDocument({
+        chat_id,
+        document: document.path
+    });
 };
 
 module.exports = { f_manda_mensaje, f_enviar_doc };
