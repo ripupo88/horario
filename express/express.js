f2c6cbef615b3b39336106d0053df4662895b3bc
ripupo88 <ripupo88@gmail.com>
2020-03-09 19:56:33 +0000

    QR implementacion

---
+++ express/express.js
@@ -0,0 +1,42 @@
+const mongo = require('../mongo/mongodb');
+const entrada = require('../comandos/entrada');
+const salida = require('../comandos/salida');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8080;
+
+app.use(bodyParser.json()); // for parsing application/json
+
+app.post('/fichar', async (req, res) => {
+   let telegram_id = req.body.telegram;
+   let empleado = await mongo.f_confirma_telegram_id(telegram_id);
+   let registro = await mongo.confirma_entrada(empleado);
+
+   let message = {
+      date: new Date(),
+      chat: {
+         id: telegram_id
+      },
+      from: {
+         id: telegram_id
+      }
+   };
+
+   if (registro[0] != undefined) {
+      salida.doSalida(message, empleado, true);
+   } else {
+      entrada.doEntrada(message, empleado, true, 'fichado por QR', 0);
+   }
+});
+
+let time = () => {
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+         resolve({ user: 'sd' });
+      }, 5000);
+   });
+};
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
