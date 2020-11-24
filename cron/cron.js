c30cc3cd5cecabea2e8ef2d30b709e9cbe6528f7
Richar <ripupo88@gmail.com>
2020-11-24 04:35:15 +0000

    añadido mes sin registro
    se puede pedir informes de meses que no tengan registros

--- cron/cron.js
+++ cron/cron.js
@@ -1,54 +1,54 @@
 //cron
-const CronJob = require("cron").CronJob;
-const moment = require("moment");
+const CronJob = require('cron').CronJob;
+const moment = require('moment');
 //////////////
-const salida = require("../comandos/salida");
-const mongo = require("../mongo/mongodb");
-const informes = require("../comandos/informes");
-const enviar = require("../telegram/enviar");
+const salida = require('../comandos/salida');
+const mongo = require('../mongo/mongodb');
+const informes = require('../comandos/informes');
+const enviar = require('../telegram/enviar');
 
 new CronJob(
-    "0 30 8 1 * *",
+    '0 30 8 1 * *',
     async () => {
         informes.f_informes_todos(1, null);
     },
     null,
     true,
-    "Europe/London"
+    'Europe/London'
 );
 
 new CronJob(
-    "0 */1 * * * *",
+    '0 */1 * * * *',
     async () => {
         try {
-            let jornadas_abiertas1 = await mongo.f_fin_jornada(12);
-            jornadas_abiertas1.forEach(async (registro) => {
-                let empleado = await mongo.f_empleado_por_id(registro.empleado);
-                let telegram_id = empleado.telegram_id;
+            // let jornadas_abiertas1 = await mongo.f_fin_jornada(12);
+            // jornadas_abiertas1.forEach(async (registro) => {
+            //     let empleado = await mongo.f_empleado_por_id(registro.empleado);
+            //     let telegram_id = empleado.telegram_id;
 
-                let newTime = Math.floor(new Date() / 1000);
-                //let createTime = newTime - 14400;
+            //     let newTime = Math.floor(new Date() / 1000);
+            //     //let createTime = newTime - 14400;
 
-                let horaEntrada = moment(registro.entrada);
-                let createTime = horaEntrada.add(8, "h").unix();
+            //     let horaEntrada = moment(registro.entrada);
+            //     let createTime = horaEntrada.add(8, "h").unix();
 
-                let message = {
-                    date: createTime,
-                    chat: {
-                        id: telegram_id,
-                    },
-                    from: {
-                        id: telegram_id,
-                    },
-                };
+            //     let message = {
+            //         date: createTime,
+            //         chat: {
+            //             id: telegram_id,
+            //         },
+            //         from: {
+            //             id: telegram_id,
+            //         },
+            //     };
 
-                salida.doSalida(message, empleado, true, true);
+            //     salida.doSalida(message, empleado, true, true);
 
-                enviar.f_manda_mensaje(
-                    empleado.telegram_id,
-                    `${empleado.alias} jornada de más de 12 horas, al parecer olvidaste fichar, el sistema fichará una jornada de 8 horas.\nSi consideras que esto es un error contacte al administrador.`
-                );
-            });
+            //     enviar.f_manda_mensaje(
+            //         empleado.telegram_id,
+            //         `${empleado.alias} jornada de más de 12 horas, al parecer olvidaste fichar, el sistema fichará una jornada de 8 horas.\nSi consideras que esto es un error contacte al administrador.`
+            //     );
+            // });
 
             let jornadas_abiertas = await mongo.f_fin_jornada(8);
             jornadas_abiertas.forEach(async (registro) => {
@@ -64,5 +64,5 @@ new CronJob(
     },
     null,
     true,
-    "Europe/London"
+    'Europe/London'
 );
