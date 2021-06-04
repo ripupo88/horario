87dbf1be28048a666a3383a037cfafb6d9c36922
Richar Pupo <ripupo88@gmail.com>
2021-06-04 06:54:11 +0100

    56version

--- comandos/comandos.js
+++ comandos/comandos.js
@@ -6,41 +6,41 @@ const empresa = require('./empresa');
 const ahora = require('./ahora');
 const app = require('./app');
 
-let f_procesa_comando = message => {
-   let re = /^\/[a-z]*/gi;
-   let result = re.exec(message.text);
+let f_procesa_comando = (message) => {
+    let re = /^\/[a-z]*/gi;
+    let result = re.exec(message.text) | [0];
 
-   switch (result[0]) {
-      case '/crear':
-         crear.f_procesa_crear(message);
-         break;
+    switch (result[0]) {
+        case '/crear':
+            crear.f_procesa_crear(message);
+            break;
 
-      case '/entrada':
-         entrada.f_procesa_entrada(message);
-         break;
+        case '/entrada':
+            entrada.f_procesa_entrada(message);
+            break;
 
-      case '/salida':
-         salida.f_procesa_salida(message);
-         break;
+        case '/salida':
+            salida.f_procesa_salida(message);
+            break;
 
-      case '/informe':
-         informes.f_procesa_informes(message);
-         break;
+        case '/informe':
+            informes.f_procesa_informes(message);
+            break;
 
-      case '/empresa':
-         empresa.f_procesa_empresa(message);
-         break;
+        case '/empresa':
+            empresa.f_procesa_empresa(message);
+            break;
 
-      case '/ahora':
-         ahora.f_procesa_ahora({ message, web: false });
-         break;
+        case '/ahora':
+            ahora.f_procesa_ahora({ message, web: false });
+            break;
 
-      case '/app':
-         app.f_procesa_app(message);
-         break;
+        case '/app':
+            app.f_procesa_app(message);
+            break;
 
-      default:
-         break;
-   }
+        default:
+            break;
+    }
 };
 module.exports = { f_procesa_comando };
